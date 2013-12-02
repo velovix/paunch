504e9c06dca483f863c8045b016849a6380cd87b
Tyler Compton <xaviosx@gmail.com>
2013-12-01 18:09:28 -0700

    Improve testing procedure.
    Combined the window_test and draw_test files into graphics_test.go, and
    standardized the error results.

---
+++ graphics_test.go
@@ -0,0 +1,132 @@
+package paunch
+
+import (
+	"testing"
+)
+
+func TestOpenWindow(t *testing.T) {
+
+	var window Window
+	err := window.Open(640, 480, "Test")
+	if err != nil {
+		t.Errorf(".Open(640, 480, \"Test\") returned %s", err)
+	}
+
+	err = window.Destroy()
+	if err != nil {
+		t.Errorf(".Destroy() returned %s", err)
+	}
+}
+
+func TestUpdateDisplay(t *testing.T) {
+
+	var window Window
+	err := window.Open(640, 480, "Test")
+	if err != nil {
+		t.Errorf(".Open(640, 480, \"Test\") returned %s", err)
+	}
+
+	err = window.UpdateDisplay()
+	if err != nil {
+		t.Errorf(".UpdateDisplay() returned %s", err)
+	}
+}
+
+func TestUpdateEvents(t *testing.T) {
+
+	var window Window
+	err := window.Open(640, 480, "Test")
+	if err != nil {
+		t.Errorf(".Open(640, 480, \"Test\") returned %s", err)
+	}
+
+	err = window.UpdateEvents()
+	if err != nil {
+		t.Errorf(".UpdateEvents() returned %s", err)
+	}
+}
+
+func TestInitDraw(t *testing.T) {
+
+	var window Window
+	err := window.Open(640, 480, "Test")
+	if err != nil {
+		t.Errorf("window.Open(640, 480, \"Test\") returned %s", err)
+	}
+
+	err = InitDraw(window)
+	if err != nil {
+		t.Errorf("draw.Init() returned %s", err)
+	}
+}
+
+func TestDrawRenderable(t *testing.T) {
+
+	triangle := []float32{
+		0.0, 0.0,
+		1.0, 0.0,
+		0.0, 1.0}
+
+	renderable, err := NewRenderable(TRIANGLES, triangle)
+	if err != nil {
+		t.Errorf("NewRenderable(TRIANGLE, triangle) returned %s", err)
+	}
+
+	err = renderable.Draw(0)
+	if err != nil {
+		t.Errorf("renderable.Draw(0) returned %s", err)
+	}
+}
+
+func TestDrawTexturedRenderable(t *testing.T) {
+
+	triangles := []float32{
+		0.0, 0.0,
+		1.0, 0.0,
+		0.0, 1.0,
+		1.0, 1.0,
+		1.0, 0.0,
+		0.0, 1.0}
+
+	texCoords := []float32{
+		0.0, 0.0,
+		1.0, 0.0,
+		0.0, 1.0,
+		1.0, 1.0,
+		1.0, 0.0,
+		0.0, 1.0}
+
+	renderable, err := NewRenderable(TRIANGLES, triangles)
+	if err != nil {
+		t.Errorf("NewRenderable(TRIANGLE, triangles) returned %s", err)
+	}
+
+	err = renderable.Texture(texCoords, "img/test.png", 2)
+	if err != nil {
+		t.Errorf("renderable.Texture(texCoords, \"img/test.png\", 2) returned %s", err)
+	}
+
+	err = renderable.Draw(0)
+	if err != nil {
+		t.Errorf("renderable.Draw(0) returned %s", err)
+	}
+}
+
+func TestEffects(t *testing.T) {
+
+	var effect Effect
+	err := effect.Init()
+	if err != nil {
+		t.Errorf("effect.Init() returned %s", err)
+	}
+
+	err = effect.New("texture", "texture/")
+	if err != nil {
+		t.Errorf("effect.New(\"texture\", \"texture/\") returned %s", err)
+	}
+
+	err = effect.Use("texture")
+	if err != nil {
+		t.Errorf("effect.Use(\"texture\") returned %s", err)
+	}
+}
