d75e087a8a5aff8742d876a7eec8b0729cc614f5
Tyler Compton <xaviosx@gmail.com>
2013-12-14 17:46:04 -0700

    Rename Collision interface to Collider.
    The name "Collider" brings to mind an interface more than "Collision".

---
+++ collider.go
@@ -0,0 +1,28 @@
+package paunch
+
+// Collider is an interface that qualifies types that can check for collisions
+// against Points, Boundings, Lines, and Polygons. It can be used to check for
+// collisions between objects without having to find out what type they are.
+type Collider interface {
+	OnPoint(*Point) bool
+	OnBounding(*Bounding) bool
+	OnLine(*Line) bool
+	OnPolygon(*Polygon) bool
+}
+
+// Collides checks if two Collider-satisfying objects are overlapping.
+func Collides(collider1, collider2 Collider) bool {
+
+	switch collider2.(type) {
+	case *Point:
+		return collider1.OnPoint(collider2.(*Point))
+	case *Bounding:
+		return collider1.OnBounding(collider2.(*Bounding))
+	case *Line:
+		return collider1.OnLine(collider2.(*Line))
+	case *Polygon:
+		return collider1.OnPolygon(collider2.(*Polygon))
+	default:
+		return false
+	}
+}
