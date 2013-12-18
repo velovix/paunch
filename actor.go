8a285041a4d97ef4381ee07ab96d3349dd1b9be0
Tyler Compton <xaviosx@gmail.com>
2013-12-17 22:30:14 -0700

    Add Actor interface for later use.

---
+++ actor.go
@@ -0,0 +1,6 @@
+package paunch
+
+// Actor is an interface to be implemented by user-defined game objects.
+// Used to make keeping track of multiple objects easier by automating the
+// process of calling methods when common events happen.
+type Actor interface{}
