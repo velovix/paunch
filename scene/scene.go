2842a0421b155a6103351a7d79775340b37e5170
Tyler Compton <xaviosx@gmail.com>
2014-05-24 21:34:13 -0700

    Add documentation to Scene package.

--- scene/scene.go
+++ scene/scene.go
@@ -6,29 +6,42 @@ import (
 	"io"
 )
 
+// Loader is an interface for objects that are able to respond to data from a
+// Scene object. An implementation of LoadScene is expected to return true if
+// the data it recieved should no longer be sent to other Loader objects.
 type Loader interface {
 	LoadScene(map[string]interface{}) bool
 }
 
+// Saver is an interface for objects that are able to supply data for a Scene
+// object.
 type Saver interface {
 	SaveScene() map[string]interface{}
 }
 
+// Scene is an object that manages the saving of game states on an
+// object-to-object basis into a JSON file.
 type Scene struct {
 	loaders []Loader
 	savers  []Saver
 }
 
+// SetLoadSceners sets the list of Loader objects that will be prompted during
+// a call to Load.
 func (scene *Scene) SetLoadSceners(loaders []Loader) {
 
 	scene.loaders = loaders
 }
 
+// SetSaveSceners sets the list of Saver objects that will be prompted during
+// a call to Save.
 func (scene *Scene) SetSaveSceners(savers []Saver) {
 
 	scene.savers = savers
 }
 
+// Load reads the information from the given JSON file and gives it to the
+// Loader objects.
 func (scene *Scene) Load(r io.Reader) error {
 
 	decoder := json.NewDecoder(r)
@@ -69,6 +82,8 @@ func (scene *Scene) Load(r io.Reader) error {
 	return nil
 }
 
+// Save reads the information to the given location using data recieved from
+// the Saver objects.
 func (scene *Scene) Save(w io.Writer) error {
 
 	encoder := json.NewEncoder(w)
