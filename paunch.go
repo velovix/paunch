95efa591d43a5cdf7c29d74e1d25fd11c6d2d2e1
Tyler Compton <xaviosx@gmail.com>
2014-07-08 14:34:00 -0700

    Add GL version return to Start function.

--- paunch.go
+++ paunch.go
@@ -10,31 +10,37 @@ func init() {
 
 // Start starts the Paunch system by opening the window and beginning to
 // recieve input. Some Paunch operations may fail if this function has not yet
-// been called, including all drawing commands.
-func Start(version Version) error {
+// been called, including all drawing commands. The version variable sets how
+// Paunch initializes OpenGL. VersionOld initializes OpenGL 2.1. A few
+// Paunch functionalities are not avaliable in this version. VersionNew
+// initializes OpenGL 3.0, where all functionalities are avaliable.
+// VersionAutomatic initializes the highest OpenGL version that the graphics
+// chip will support. Start returns the resulting version and an error, if any.
+// The returned version may not be accurate if Start results in an error.
+func Start(version Version) (Version, error) {
 
 	var err error
 
 	err = initWindows()
 	if err != nil {
-		return err
+		return VersionOld, err
 	}
 	err = paunchWindow.open()
 	if err != nil {
-		return err
+		return VersionOld, err
 	}
 
-	err = initDraw(version)
+	version, err := initDraw(version)
 	if err != nil {
-		return err
+		return VersionOld, err
 	}
 
 	err = startAudio()
 	if err != nil {
-		return err
+		return paunchGLVersion, err
 	}
 
-	return nil
+	return paunchGLVersion, nil
 }
 
 // Stop safely closes the Paunch instance.
