5115b57d0fdd133f7392a0a90b7387d15c389c33
Tyler Compton <xaviosx@gmail.com>
2014-07-28 23:55:34 -0700

    Update package description.

--- paunch.go
+++ paunch.go
@@ -1,6 +1,4 @@
-// Package paunch is a 2D game engine written in Go. It uses OpenGL and other
-// cross-platform technologies like GLFW, making games built on Paunch
-// immediately portable.
+// Package paunch is a 2D game engine written in Go.
 package paunch
 
 func init() {
