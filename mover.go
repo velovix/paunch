dd1a3c73dc2ddfa114b88fa39b075cf2a9835278
Tyler Compton <xaviosx@gmail.com>
2014-12-19 02:24:00 +0000

    Update Mover interface with new naming convention.

--- mover.go
+++ mover.go
@@ -1,9 +1,9 @@
 package paunch
 
-// The Mover interface includes all structs that implement the Move and
-// SetPosition methods.
+// The Mover interface represents an object that can move and report it's
+// positions.
 type Mover interface {
 	Move(x, y float64)
 	SetPosition(x, y float64)
-	GetPosition() (x, y float64)
+	Position() (x, y float64)
 }
